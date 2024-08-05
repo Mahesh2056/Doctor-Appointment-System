694c97329095ca6e48b936ef6d4ea8e345aef758
MAHESH2056 <maheschy2323@gmail.com>
2024-08-05 16:24:00 +0545

    Create Complete User Registration

--- models/userModels.js
+++ models/userModels.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [ture, "password is require"],
+        required: [true, "password is require"],
     },
 });
 

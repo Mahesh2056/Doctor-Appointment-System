296a5d123d66a0da0e06c6cac8edae28f53967d0
MAHESH2056 <maheschy2323@gmail.com>
2024-08-06 05:34:45 +0545

    Create React Toolkit setup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "../styles/RegisterStyles.css";
 import { Form, Input, message } from "antd";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
 //form handler
 const onfinishHandler = async (values) => {
   try {
+    dispatch(showLoading());
     const res = await axios.post("/api/v1/user/login", values);
+    dispatch(hideLoading());
     if (res.data.success) {
       localStorage.setItem("token", res.data.token);
       message.success("Login Successfully");
@@ -19,6 +24,7 @@ const onfinishHandler = async (values) => {
       message.error(res.data.message);
     }
   } catch (error) {
+    dispatch(hideLoading());
     console.log(error);
     message.error("something went wrong");
   }

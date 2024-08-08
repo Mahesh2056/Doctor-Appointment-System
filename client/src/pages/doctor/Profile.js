8cd640688fdefec9700ec53b4ca9f0fa8ae010ba
MAHESH2056 <maheschy2323@gmail.com>
2024-08-08 18:36:02 +0545

    Design Get Doctor Profile

---
+++ client/src/pages/doctor/Profile.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Profile = () => {
+  const { user } = useSelector((state) => state.user);
+  const [doctor, setDoctor] = useState(null);
+  const params = useParams();
+
+  //getDoc Details
+  const getDoctorInfo = async () => {
+    try {
+      const res = await axios.post(
+        "/api/v1/doctor/getDoctorInfo",
+        { userId: params.id },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (res.data.success) {
+        setDoctor(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDoctorInfo();
+     //eslint-disable-next-line
+  }, []);
+
+  return (
+    <Layout>
+        <h1>Manage Profile</h1>
+    </Layout>
+  );
+};
+
+export default Profile;
